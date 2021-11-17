078f37efc8c09f3c4aa6663073920e2b7962f87f
SwartzKing <dswartzy0131@gmail.com>
2021-11-17 16:37:11 -0500

    first commit

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2d.AddForce(direction * force);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        EnemyScript e = other.collider.GetComponent<EnemyScript>();
+        if (e != null)
+        {
+            e.Fix();
+        }
+        
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        if(transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
